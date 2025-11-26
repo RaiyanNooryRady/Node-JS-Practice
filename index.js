3316ebdd0b01eba973d823242aac6329feaccd52
raiyannooryrady <raiyannooryrady@gmail.com>
2025-11-26 12:16:08 +0600

    Enhance index.js with lodash usage: added variable declarations and console logs for type checking and array manipulation.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world test ds great');
+const _=require('lodash');
+const x='abc';
+const y=123;
+const z= [2,3,4,5];
+console.log(_.now());
+console.log(`x is string: `,_.isString(x));
+console.log(`y is string: `,_.isString(y));
+console.log(`Last value of array z: `, _.last(z));
