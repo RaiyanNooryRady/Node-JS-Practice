44f9c80a27bc79711623022cb3e99e0a3bd58048
raiyannooryrady <raiyannooryrady@gmail.com>
2025-11-26 16:21:06 +0600

    Update index.js to import people module from test directory; uncomment lodash usage for last element retrieval and comment out previous console logs.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const _ = require("lodash");
+const _ = require("lodash");
 // const x = "abc";
 // const y = 123;
 // const z = [2, 3, 4, 5];
-// console.log(_.now());
+//console.log(_.now());
 // console.log(`x is string: `, _.isString(x));
 // console.log(`y is string: `, _.isString(y));
 // console.log(`Last value of array z: `, _.last(z));
@@ -12,9 +12,10 @@
 //console.log(global); 
 // console.log(__dirname)
 //console.log(__filename)
-const people= require('./people');
+const people= require('./test/people');
 //console.log(people.test())
-console.log(people.a);
-people.test();
-console.log(`people:`, people.people);
-console.log('hello')
+// console.log(people.a);
+// people.test();
+// console.log(`people:`, people.people);
+// console.log('hello')
+console.log('people last element:', _.last(people.people));
